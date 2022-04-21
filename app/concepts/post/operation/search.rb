ba497b6306616090170a562f82191d203b2bb6f1
nangthinzarphyo@cgm-myanmar.com <nangthinzarphyo@cgm-myanmar.com>
2022-04-21 09:19:33 +0630

    search order

--- app/concepts/post/operation/search.rb
+++ app/concepts/post/operation/search.rb
@@ -3,7 +3,7 @@ module Post::Operation
         step :search_post_list
         def search_post_list(options,**)
             searchKey = options[:params][:searchInfo]
-            options['posts'] = Post.joins(:user).where("lower (posts.title) LIKE :value OR lower (posts.description) LIKE :value OR lower (users.name) LIKE :value", value: "%#{searchKey}%" ) 
+            options['posts'] = Post.joins(:user).where("lower (posts.title) LIKE :value OR lower (posts.description) LIKE :value OR lower (users.name) LIKE :value", value: "%#{searchKey}%" ).order("posts.updated_at DESC") 
         end
     end
 end
